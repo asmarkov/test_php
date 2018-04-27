929a3de0ed97217140faa1b756767d79ebb811ed
asmarkov <asmarkov93@gmail.com>
2018-04-27 22:35:39 +0300

    add model and controller methods. add routes.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Cities;
+use App\Qualifications;
+use App\Users;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,4 +14,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $qualifications = Qualifications::all();
+        $cities = Cities::all();
+        $users_joined_table = Users::joinTables();
+        $users = Users::groupData($users_joined_table);
+        return view('index',compact("users","cities","qualifications"));
+    }
+
+    public function ajax(Request $request){
+        $parameters = $request->all();
+        if(empty($parameters)){
+            $users_joined_table = Users::joinTables();
+            $users = Users::groupData($users_joined_table);
+            return view('users/table', compact("users"));
+        }
+        $users_id = Users::getUsersIdByRequest($parameters);
+        $users_joined_table = Users::joinTables()->whereIn("users.id",$users_id);
+        $users = Users::groupData($users_joined_table);
+        return view('users/table', compact("users"));
+    }
 }
--- app/Users.php
+++ app/Users.php
@@ -2,9 +2,38 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Users extends Model
 {
-    //
+    public static function joinTables() :Builder
+    {
+        return self::query()->select("users.name","cities.name as city_name","qualifications.name as qualifications_name")
+            ->join('qualifications', 'users.qualification_id', '=', 'qualifications.id')
+            ->join('users_cities', 'users_cities.user_id', '=', 'users.id')
+            ->join('cities', 'users_cities.city_id', '=', 'cities.id');
+    }
+
+    public static function groupData(Builder $users)
+    {
+        $users = $users->get()->groupBy("name");
+        foreach ($users as $key => $collection) {
+            $users[$key] = $collection->groupBy("qualifications_name");
+        }
+        return $users;
+    }
+
+    public static function getUsersIdByRequest($parameters)
+    {
+        $users = Users::query();
+        if (isset($parameters["cities"]) && !empty($parameters["cities"]) && is_array($parameters["cities"])) {
+            $users = $users->join('users_cities', 'users_cities.user_id', '=', 'users.id');
+            $users = $users->whereIn("users_cities.city_id", $parameters["cities"]);
+        }
+        if (isset($parameters["qualification"]) && !empty($parameters["qualification"]) && is_array($parameters["qualification"])) {
+            $users = $users->whereIn("users.qualification_id", $parameters["qualification"]);
+        }
+        return $users->pluck("users.id");
+    }
 }
